4e0099a8fae7a7f4834b2b01f1c0e196de26daba
faizm10 <faizmustansar10@gmail.com>
2024-06-07 21:16:45 -0400

    minor error

--- basilyusuf/src/app/about/page.tsx
+++ basilyusuf/src/app/about/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 import React from "react";
-import { Tab } from "@headlessui/react";
-import { motion } from "framer-motion";
-import Image from "next/image";
-import { Accordion, AccordionItem, Card, CardBody } from "@nextui-org/react";
+
 const AboutMeSection: React.FC = () => {
   return (
     <>
       <div className="flex h-screen w-full">
-        <div className="bg-yellow-400  p-4 text-black flex flex-col justify-center items-center">
+        <div className="bg-yellow-400 p-4 text-black flex flex-col justify-center items-center">
           <div className="align-middle">
             <h2 className="text-8xl font-bold mb-4">About Me</h2>
-            <p className="text-lg mb-6 font-sans">Hi, I'm Basil</p>
+            <p className="text-lg mb-6 font-sans">Hi, I am Basil</p>
           </div>
         </div>
       </div>
-
     </>
   );
 };
